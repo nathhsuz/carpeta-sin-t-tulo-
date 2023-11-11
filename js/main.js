fd2a0ad0308aba51e6f8b621f7d25891b0fe5545
Natalia <natalia@MacBook-Air-de-Natalia.local>
2023-11-11 15:23:30 -0500

    final

--- js/main.js
+++ js/main.js
@@ -1,258 +1,134 @@
-const productos = [
-    {
-        id: "Patines-01",
-        titulo: "Abrigo 01",
-        imagen: "./img/pat1.webp",
-        categoria: {
-            nombre: "Patines",
-            id: "patines",
-        },
-        precio: 699
-    },
-    {
-        id: "Patines-02",
-        titulo: "Patines 02",
-        imagen: "./img/abrigos/02.jpg",
-        categoria: {
-            nombre: "Patines",
-            id: "patines",
-        },
-        precio: 800
-    },
-    {
-        id: "Patines-03",
-        titulo: "Abrigo 03",
-        imagen: "./img/patt3.png",
-        categoria: {
-            nombre: "Patines",
-            id: "patines",
-        },
-        precio: 845
-    },
+let productos = [];
 
-    {
-        id: "patines04",
-        titulo: "Patines 04",
-        imagen: "./img/patt4.png",
-        categoria: {
-            nombre: "Patines",
-            id: "patines",
-        },
-        precio, 1000
-     },
-
-    {
-        id: "patines05",
-        titulo: "Patines 05",
-        imagen: "./img/patt5.png",
-        categoria: {
-            nombre: "Patines",
-            id: "patines",
-        },
-        precio: 1200
-    },
-
-
-    {
-        id: "patines05",
-        titulo: "Patines 05",
-        imagen: "./img/patt5.png",
-        categoria: {
-            nombre: "Patines",
-            id: "patines",
-        },
-        precio: 1200
-    },
-
-    {
-        id: "Patines-06",
-        titulo: "Patines 06",
-        imagen: "./img/patt6.png",
-        categoria: {
-            nombre: "Patines",
-            id: "patines",
-
-        }   precio: 1000,
-
-    },
-    // Protect/Pads
-    {
-        id: "protecciones01",
-        titulo: "Protecciones-01",
-        imagen: "./img/proteccioness.png",
-        categoria: {
-            nombre: "Protecciones/pads",
-            id: "protecciones/pads",
-        },
-        precio: 300
-    },
-    {
-        id: "Protecciones-02",
-        titulo: "Protecciones 02",
-        imagen: "./img/camisetas/02.jpg",
-        categoria: {
-            nombre: "Protecciones/Pads",
-            id: "protecciones/Pads",
-        },
-        precio: 450
-    },
-    {
-        id: "Ruedas-03",
-        titulo: "Ruedas 03",
-        imagen: "./img/w1.png",
-        categoria: {
-            nombre: "protecciones/Pads",
-            id: "protecciones/Pads",
-        },
-        precio: 250
-    },
-    {
-        id: "Ruedas-04",
-        titulo: "Ruedas 04",
-        imagen: "./img/w2.png",
-        categoria: {
-            nombre: "Protecciones/pads",
-            id: "protecciones/pads",
-        },
-        precio: 280
-    },
-    {
-        id: "Ruedas-05",
-        titulo: "Ruedas 05",
-        imagen: "./img/w3.png",
-        categoria: {
-            nombre: "Protecciones/pads",
-            id: "protecciones/pads",
-
-        }, precio: 250
-    },
-    {
-
-
-
-        id: "Accesorios-01",
-        titulo: "Accesorios01",
-        imagen: "./img/pantalones/01.jpg",
-        categoria: {
-            nombre: "Accesorios",
-            id: "accesorios",
-        },
-        precio: 300
-    },
-    {
-        id: "Accesorios-02",
-        titulo: "Accesorios 02",
-        imagen: "./img/merch1.png",
-        categoria: {
-            nombre: "Accesorios",
-            id: "accesorios",
-        },
-        precio: 1000
-    },
-    {
-        id: "Accesorios-03",
-        titulo: "Accesorios 03",
-        imagen: "./img/merch2.png",
-        categoria: {
-            nombre: "Accesorios",
-            id: "accesorios",
-        },
-        precio: 1000
-    },
-    {
-        id: "Accesorios-04",
-        titulo: "Accesorios 04",
-        imagen: "./img/merch3.png",
-        categoria: {
-            nombre: "Accesorios",
-            id: "Accesorios"
-        },
-        precio: 1000
-    },
-    {
-        id: "Accesorios",
-        titulo: "Pantalón 05",
-        imagen: "./img/merch4.png",
-        categoria: {
-            nombre: "Accesorios",
-            id: "accesorios"
-        },
-        precio: 1000
-    }
-];
-
-
-
-
+fetch("./js/productos.json")
+    .then(response => response.json())
+    .then(data => {
+        productos = data;
+        cargarProductos(productos);
+    })
 
 
 const contenedorProductos = document.querySelector("#contenedor-productos");
-const botonCategorias = document.querySelectorAll(".boton-categorias");
-const tituloPrincipal = document.querySelector("titulo-principal");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
 let botonesAgregar = document.querySelectorAll(".producto-agregar");
-const numerito = document.querySelector("#numerito")
+const numerito = document.querySelector("#numerito");
 
-function cargarProductos() {
-    productos.forEach(productos => (
-        let, div = document.createElement("div")
-        , div.classList.add("producto")
-        , div.innerHTML = ` 
-    <div class="producto-detalle">
-    <img class="producto-imagen" src="${productos.imagen}" alt="">
-    <div>
-        <h3 class="producto-titulo">${productos.titulo}</h3>
-        <p class="producto-precio">${productos.precio}</p>
-        <button class="producto-agregar" id="${productos.id}"Agregar</button>
 
-    </div>
-</div> 
-`
-        , contenedorProductos.append(div)
-    )
-    )
+botonesCategorias.forEach(boton => boton.addEventListener("click", () => {
+    aside.classList.remove("aside-visible");
+}))
+
+
+function cargarProductos(productosElegidos) {
+
+    contenedorProductos.innerHTML = "";
+
+    productosElegidos.forEach(producto => {
+
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio">$${producto.precio}</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div>
+        `;
+
+        contenedorProductos.append(div);
+    })
+
+    actualizarBotonesAgregar();
 }
 
-cargarProductos(productos);
 
-botonCategoria.forEach(boton => {
+botonesCategorias.forEach(boton => {
     boton.addEventListener("click", (e) => {
 
-        botonCategoria.forEach(boton => boton.classList.remove("active"));
-
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
         e.currentTarget.classList.add("active");
 
         if (e.currentTarget.id != "todos") {
-            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTraget.id);
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
             cargarProductos(productosBoton);
         } else {
             tituloPrincipal.innerText = "Todos los productos";
             cargarProductos(productos);
+        }
 
-        } 
-   )
+    })
 });
 
-function actualizarBotonesAgregar{} { 
+function actualizarBotonesAgregar() {
     botonesAgregar = document.querySelectorAll(".producto-agregar");
-botonesAgregar.forEach(boton => 
-{
-    boton.addEventListener("Click", agregarAlCarrito);
-});
 
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
 }
 
-const productosEnCarrito = [];
+let productosEnCarrito;
+
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+if (productosEnCarritoLS) {
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumerito();
+} else {
+    productosEnCarrito = [];
+}
 
 function agregarAlCarrito(e) {
-const idBoton = e.currentTarget.id;
-const productoAgregado = productos.find(producto => producto.id === idBoton);
 
-if(productosEnCarrito.some(producto => producto.id === idBoton))
-productosEnCarrito [index].cantidad++;
+    Toastify({
+        text: "Producto agregado",
+        duration: 3000,
+        close: true,
+        gravity: "top", 
+        position: "right",
+        stopOnFocus: true, 
+        style: {
+          background: "linear-gradient(to right, #4b33a8, #785ce9)",
+          borderRadius: "2rem",
+          textTransform: "uppercase",
+          fontSize: ".75rem"
+        },
+        offset: {
+            x: '1.5rem', 
+            y: '1.5rem',
+          },
+        onClick: function(){} 
+      }).showToast();
 
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
 
-else {
-    productoAgregado.cantidad = 1;
-    productosEnCarrito.push(productoAgregado)
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    } else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
+
+    actualizarNumerito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
 }
+
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
 }
+
+
+
+
+
+
+
+
